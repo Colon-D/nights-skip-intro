fc0c4968a96ea79c7fdc224406133bd1af267cb5
Colon-D <JJPTails@gmail.com>
2023-06-26 20:48:32 +0100

    allow skipping to dream

--- Config.cs
+++ Config.cs
@@ -5,35 +5,104 @@ namespace nights.test.skipintro.Configuration;
 
 public class Config : Configurable<Config>
 {
-    /*
-        User Properties:
-            - Please put all of your configurable properties here.
-    
-        By default, configuration saves as "Config.json" in mod user config folder.    
-        Need more config files/classes? See Configuration.cs
-    
-        Available Attributes:
-        - Category
-        - DisplayName
-        - Description
-        - DefaultValue
+	/*
+		User Properties:
+			- Please put all of your configurable properties here.
+	
+		By default, configuration saves as "Config.json" in mod user config folder.    
+		Need more config files/classes? See Configuration.cs
+	
+		Available Attributes:
+		- Category
+		- DisplayName
+		- Description
+		- DefaultValue
 
-        // Technically Supported but not Useful
-        - Browsable
-        - Localizable
+		// Technically Supported but not Useful
+		- Browsable
+		- Localizable
 
-        The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
-    */
+		The `DefaultValue` attribute is used as part of the `Reset` button in Reloaded-Launcher.
+	*/
 
-    [DisplayName("Skip to Gamemode")]
-    [Description("Gamemode to skip to when loading Intro (Advertise).")]
-    [DefaultValue(Gamemode.MainMenu)]
-    public Gamemode SkipToGamemode { get; set; } = Gamemode.MainMenu;
+	[Category("General")]
+	[DisplayName("Skip to GameMode")]
+	[Description("GameMode to skip to when loading the game.")]
+	[DefaultValue(GeneralGameModes.Title)]
+	public GeneralGameModes SkipToGameMode { get; set; } = GeneralGameModes.Title;
+	
+	[Category("General")]
+	[DisplayName("GameMode Game: Dream")]
+	[Description("Dream to load when skipping to the Game GameMode.\n" +
+		"Does not support skipping to the Mare part of the dream,\n" +
+		"or skipping into a Sega Saturn dream.\n" +
+		"I would like to add those in the future though.")]
+	[DefaultValue(DreamPiaOnly.SpringValley)]
+	public DreamPiaOnly SkipToDream { get; set; } = DreamPiaOnly.SpringValley;
 
-    [DisplayName("Skip to Gamemode integer override")]
-    [Description("Override for enum (-1 to not use).")]
-    [DefaultValue(-1)]
-    public int SkipToGamemodeInt { get; set; } = -1;
+	[Category("Debugging")]
+	[DisplayName("Skip to GameMode (debugging)")]
+	[Description("Many of these do nothing, or crash the game.")]
+	[DefaultValue(GameMode.UseGeneralGameMode)]
+	public GameMode SkipToGameModeDebugging { get; set; } = GameMode.UseGeneralGameMode;
+}
+
+public enum GeneralGameModes {
+	Title = 8,
+	MainMenu = 10,
+	DreamSelect = 11,
+	Game = 30,
+}
+
+public enum DreamPiaOnly {
+	SpringValley = 1,
+	SplashGarden = 3,
+	MysticForest = 5,
+	FrozenBell = 7,
+	SoftMuseum = 9,
+	StickCanyon = 11,
+	TwinSeedsClaris = 13,
+	TwinSeedsElliot = 15,
+	SpringValleyXmasClaris = 17,
+	SpringValleyXmasElliot = 19,
+};
+
+// many of these do nothing
+public enum GameMode {
+	UseGeneralGameMode = -1,
+	Unknown = 0, // referenced in 0x55C790
+	Progressive = 1,// (black screen)
+	MemoryCard = 2, // (black screen)
+	MemoryCardSaving = 3, // GameMode_MemoryCard
+	MemoryCardReplay = 4, // GameMode_MemoryCard
+	Advertise = 5, // GameMode_SegaLogo, game seems to start with this
+	CutsceneIntro = 6, // GameMode_Movie
+	CutsceneStoryBegin = 7, // GameMode_Demo
+	Title = 8, // GameMode_Title
+	LoadSelect = 9, // (black screen)
+	MainMenu = 10, // GameMode_ModeSelect
+	DreamSelect = 11, // GameMode_SelectDiary
+	PS2Option = 12, // GameMode_Option
+	Gallery = 13, // GameMode_Gallery
+	MovieTheatre = 14, // GameMode_MovieTheater
+	NightopianCollection = 15, // GameMode_Collection
+	GameMode0x10 = 16, // referenced in 0x55C790
+	GameMode0x11 = 17, // referenced in 0x55C790
+	Quit = 18, // 
+	GameCrash = 19, // GameMode_Game
+	GameReplay = 20, // GameMode_Game
+	CutsceneStoryEnd = 21, // GameMode_Ending
+	CutsceneChristmasBegin = 22, // GameMode_Movie
+	CutsceneChristmasEnd = 23, // GameMode_XmasEnding
+	Hint = 24, // GameMode_Hint
+	TrialUnlockFullGame = 25, // GameMode_TrialXBOX360
+	PiaOver = 26, // GameMode_GameOver
+	MareOver = 27, // GameMode_GameOver
+	SizeChanger = 28,
+	SizeChanger_2 =  29,
+	Game = 30, // GameMode_Game
+	MainMenuRedirect = 31,
+	Logo_Loading = 32
 }
 
 /// <summary>
@@ -42,5 +111,5 @@ public class Config : Configurable<Config>
 /// </summary>
 public class ConfiguratorMixin : ConfiguratorMixinBase
 {
-    // 
+	// 
 }
--- Mod.cs
+++ Mod.cs
@@ -3,6 +3,7 @@ using nights.test.skipintro.Template;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.X86;
 using Reloaded.Mod.Interfaces;
+using System.Runtime.InteropServices;
 using CallingConventions = Reloaded.Hooks.Definitions.X86.CallingConventions;
 using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
 
@@ -13,45 +14,45 @@ namespace nights.test.skipintro;
 /// </summary>
 public class Mod : ModBase // <= Do not Remove.
 {
-    /// <summary>
-    /// Provides access to the mod loader API.
-    /// </summary>
-    private readonly IModLoader _modLoader;
+	/// <summary>
+	/// Provides access to the mod loader API.
+	/// </summary>
+	private readonly IModLoader _modLoader;
 
-    /// <summary>
-    /// Provides access to the Reloaded.Hooks API.
-    /// </summary>
-    /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
-    private readonly IReloadedHooks? _hooks;
+	/// <summary>
+	/// Provides access to the Reloaded.Hooks API.
+	/// </summary>
+	/// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
+	private readonly IReloadedHooks? _hooks;
 
-    /// <summary>
-    /// Provides access to the Reloaded logger.
-    /// </summary>
-    private readonly ILogger _logger;
+	/// <summary>
+	/// Provides access to the Reloaded logger.
+	/// </summary>
+	private readonly ILogger _logger;
 
-    /// <summary>
-    /// Entry point into the mod, instance that created this class.
-    /// </summary>
-    private readonly IMod _owner;
+	/// <summary>
+	/// Entry point into the mod, instance that created this class.
+	/// </summary>
+	private readonly IMod _owner;
 
-    /// <summary>
-    /// Provides access to this mod's configuration.
-    /// </summary>
-    private Config _configuration;
+	/// <summary>
+	/// Provides access to this mod's configuration.
+	/// </summary>
+	private Config _configuration;
 
-    /// <summary>
-    /// The configuration of the currently executing mod.
-    /// </summary>
-    private readonly IModConfig _modConfig;
+	/// <summary>
+	/// The configuration of the currently executing mod.
+	/// </summary>
+	private readonly IModConfig _modConfig;
 
-    public Mod(ModContext context)
-    {
-        _modLoader = context.ModLoader;
-        _hooks = context.Hooks;
-        _logger = context.Logger;
-        _owner = context.Owner;
-        _configuration = context.Configuration;
-        _modConfig = context.ModConfig;
+	public Mod(ModContext context)
+	{
+		_modLoader = context.ModLoader;
+		_hooks = context.Hooks;
+		_logger = context.Logger;
+		_owner = context.Owner;
+		_configuration = context.Configuration;
+		_modConfig = context.ModConfig;
 
 
 		// For more information about this template, please see
@@ -60,54 +61,107 @@ public class Mod : ModBase // <= Do not Remove.
 		// If you want to implement e.g. unload support in your mod,
 		// and some other neat features, override the methods in ModBase.
 
-        // read from config
-		if (_configuration.SkipToGamemodeInt == -1) {
-			_skipToGamemode = _configuration.SkipToGamemode;
+		// read from config
+		if (_configuration.SkipToGameModeDebugging == GameMode.UseGeneralGameMode) {
+			_skipToGameMode = (GameMode)_configuration.SkipToGameMode;
 		} else {
-			_skipToGamemode = (Gamemode)_configuration.SkipToGamemodeInt;
+			_skipToGameMode = (GameMode)_configuration.SkipToGameModeDebugging;
 		}
 
 		// hook gameplay transition function
 		unsafe {
-            _gamemodeTransitionHook = _hooks.CreateHook<GamemodeTransition>(
-                GamemodeTransitionImpl, 0x489210
-            ).Activate();
-        }
+			_transitionHook = _hooks.CreateHook<Transition>(
+				GameModeTransitionImpl, 0x489210
+			).Activate();
+		}
 	}
 
-	IHook<GamemodeTransition> _gamemodeTransitionHook;
-    Gamemode _skipToGamemode;
-    bool _skipped = false; // can only skip once, probably unneeded
-	unsafe int GamemodeTransitionImpl(void* self) {
-        if (!_skipped) {
-			// unused, but nice to know
-			Gamemode* at = (Gamemode*)((byte*)self+0x8);
-			Gamemode* going_to = (Gamemode*)((byte*)self+0x10);
-
-		    if (*going_to == Gamemode.Advertise) {
-				*going_to = _skipToGamemode;
-				_skipped = true;
-		    }
-        }
-
-		return _gamemodeTransitionHook.OriginalFunction.Invoke(self);
-	}
 	[Function(CallingConventions.MicrosoftThiscall)]
-	unsafe delegate int GamemodeTransition(void* self);
+	unsafe delegate int Transition(GameModeManager* self);
+	IHook<Transition> _transitionHook;
+	GameMode _skipToGameMode;
+	bool _skipped = false; // can only skip once, probably unneeded
+	unsafe int GameModeTransitionImpl(GameModeManager* gamemode_manager) {
+		if (!_skipped) {
+			if (gamemode_manager->to == GameMode.Advertise) {
+				gamemode_manager->to = _skipToGameMode;
+
+				if (_skipToGameMode == GameMode.Game) {
+					GameStateManager* gameStateManager =
+						*(GameStateManager**)0x24C4E94;
+					gameStateManager->Dream =
+						(Dream)_configuration.SkipToDream;
+
+					if (
+						gameStateManager->Dream
+						>= Dream.SpringValleyXmasClaris
+					) {
+						DreamType* dreamType = (DreamType*)0x8B13C8;
+						*dreamType = DreamType.Christmas;
+					}
+				}
+
+				_skipped = true;
+			}
+		}
+
+		return _transitionHook.OriginalFunction.Invoke(gamemode_manager);
+	}
 
 	#region Standard Overrides
 	public override void ConfigurationUpdated(Config configuration)
-    {
-        // Apply settings from configuration.
-        // ... your code here.
-        _configuration = configuration;
-        _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
-    }
-    #endregion
+	{
+		// Apply settings from configuration.
+		// ... your code here.
+		_configuration = configuration;
+		_logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
+	}
+	#endregion
 
-    #region For Exports, Serialization etc.
+	#region For Exports, Serialization etc.
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public Mod() { }
+	public Mod() { }
 #pragma warning restore CS8618
-    #endregion
+	#endregion
 }
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct GameModeManager {
+	[FieldOffset(0x10)]
+	public GameMode to;
+}
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct GameStateManager {
+	[FieldOffset(0x44)]
+	public Dream Dream;
+}
+
+public enum DreamType {
+	BrandNew,
+	SegaSaturn,
+	Christmas,
+}
+
+public enum Dream {
+	SpringValley = 1,
+	Gillwing,
+	SplashGarden,
+	Puffy,
+	MysticForest,
+	Gulpo,
+	FrozenBell,
+	Clawz,
+	SoftMuseum,
+	Jackle,
+	StickCanyon,
+	Reala,
+	TwinSeedsClaris,
+	WizemanClaris,
+	TwinSeedsElliot,
+	WizemanElliot,
+	SpringValleyXmasClaris,
+	GillwingXmasClaris,
+	SpringValleyXmasElliot,
+	GillwingXmasElliot,
+};
